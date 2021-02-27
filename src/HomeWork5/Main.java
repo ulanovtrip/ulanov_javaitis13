98e7a1f90478153fa694d304233156fcca5b69a1
iulanov <iulanov@swiftcom.uk>
2021-02-27 22:29:45 +0300

    rewrite Sequence + iterator

--- src/HomeWork5/Main.java
+++ src/HomeWork5/Main.java
@@ -2,30 +2,51 @@ package HomeWork5;
 
 public class Main {
     public static void main(String[] args) {
-        SequenceGenerator generator = new SequenceGenerator("First", 10, 20);
+        SequenceGenerator generator = new SequenceGenerator("First", 3, 20);
         Sequence sequence = generator.generateSequence();
         SequenceObserver subscriber = new SequenceObserver(sequence);
         sequence.addObserver(subscriber);
 
-        sequence.add(7);
-        sequence.deleteByIndex(7);
-        sequence.deleteByValue(5);
-        sequence.getByIndex(7);
-        sequence.insert(5, 777);
-        sequence.replace(3, 13);
+        System.out.println("----------------------------------------------------------------------------------------");
+        sequence.add(1);
+        sequence.add(12);
+        sequence.add(2);
+        sequence.add(3);
+        sequence.add(4);
+        sequence.deleteByIndex(3);
+        sequence.deleteByValue(12);
+        sequence.add(77);
+        sequence.getByIndex(3);
+        sequence.replace(3, 88);
+        sequence.insert(0, 13);
+
+        Sequence.SequenceIterator iterator = sequence.new SequenceIterator();
+        iterator.printSequence();
+
         sequence.removeObserver(subscriber);
         System.out.println("----------------------------------------------------------------------------------------");
 
-        SequenceGenerator generator2 = new SequenceGenerator("Second", 30, 25);
+        SequenceGenerator generator2 = new SequenceGenerator("Second", 5, 15);
         Sequence sequence2 = generator2.generateSequence();
         SequenceObserver subscriber2 = new SequenceObserver(sequence2);
-        sequence.addObserver(subscriber2);
+        sequence2.addObserver(subscriber2);
 
-        sequence.add(5);
-        sequence.deleteByIndex(5);
-        sequence.getByIndex(5);
-        sequence.insert(9, 555);
-        sequence.replace(5, 13);
-        sequence2.removeObserver(subscriber2);
+        sequence2.add(55);
+        sequence2.add(32);
+        sequence2.add(11);
+        sequence2.add(7);
+        sequence2.add(3);
+        sequence2.deleteByIndex(2);
+        sequence2.deleteByValue(55);
+        sequence2.add(111);
+        sequence2.getByIndex(5);
+        sequence2.replace(1, 444);
+        sequence2.insert(3, 999);
+
+        Sequence.SequenceIterator iterator2 = sequence2.new SequenceIterator();
+        iterator2.printSequence();
+
+        sequence.removeObserver(subscriber2);
+        System.out.println("----------------------------------------------------------------------------------------");
     }
 }
--- src/HomeWork5/Sequence.java
+++ src/HomeWork5/Sequence.java
@@ -1,21 +1,16 @@
 package HomeWork5;
 
 // класс для работы с последовательностью чисел
-
-
-
 // вставить элемент со сдвигом всех остальных - insert()
 
-
 import java.util.ArrayList;
-import java.util.Random;
+import java.util.stream.IntStream;
 
 public class Sequence implements Observable {
 
-    Random random = new Random();
     private ArrayList<Observer> observers = new ArrayList<>();
     private int size;
-    private ArrayList<Integer> sequence;
+    private int[] sequence;
     private String sequenceId;
     private int bound;
 
@@ -23,53 +18,122 @@ public class Sequence implements Observable {
         this.bound = bound;
         this.size = size;
         this.sequenceId = sequenceId;
-        this.sequence = generateRandomNumbers();
+        generateRandomNumbers();
     }
 
-    private ArrayList<Integer> generateRandomNumbers() {
-        ArrayList<Integer> temp = new ArrayList<>();
+    //заполняем последовательность -1, что будет значить, что она пуста
+    private void generateRandomNumbers() {
+        sequence = new int[size];
         for (int i = 0; i < size; i++) {
-            temp.add(random.nextInt(10));
+            sequence[i] = -1;
         }
-        return temp;
     }
 
     // добавить число в последовательность
     public void add(int num) {
-        sequence.add(num);
+        for (int i = 0; i < sequence.length; i++) {
+            if (sequence[i] == -1) {
+                sequence[i] = num;
+                break;
+            }
+
+            //если прошлись и не нашли места для вставки
+            if (i == sequence.length - 1) {
+                //копируем во временный массив
+                int[] temp = new int[sequence.length];
+                for (int j = 0; j < sequence.length; j++) {
+                    temp[j] = sequence[j];
+                }
+                //увеличиваем размер последовательности
+                sequence = new int[size * 2];
+                //добиваем новые пустые ячейки значением пустоты, т.е. -1
+                sequence = IntStream.generate(() -> -1).limit(sequence.length).toArray();
+                for (int j = 0; j < temp.length; j++) {
+                    sequence[j] = temp[j];
+                }
+                sequence[sequence.length - temp.length] = num;
+                break;
+            }
+        }
         notifyObserver("ADD : ", num);
     }
 
     // удалить число из последовательности по индексу
     // 3, 4, 5 -> removeByIndex(1) -> 3, 5
     public void deleteByIndex(int index) {
-        int deletedNm = sequence.remove(index);
-        notifyObserver("deleteByIndex", deletedNm);
+        //запомним число под индексом для вывода
+        int remembered = sequence[index];
+        //запишем вместо удаляемого числа -2, чтобы поймать его потом.
+        sequence[index] = -2;
+        //создаю временный массив -1 эелемент
+        sequence = shiftSequence();
+        notifyObserver("deleteByIndex", remembered);
+    }
+
+    public int[] shiftSequence() {
+        int[] temp = new int[sequence.length - 1];
+
+        //переменная для индексов временного массива
+        int numTempCell = 0;
+        for (int i = 0; i < sequence.length; i++) {
+            //когда пападается число -2 в исходной последовательности
+            if (sequence[i] == -2) {
+                //пропускаю его и пишу во временный массив следующий за ним эелемент
+                temp[numTempCell++] = sequence[++i];
+            } else {
+                temp[numTempCell++] = sequence[i];
+            }
+        }
+        // присваиваю исходной последовательности значение временного с удалённым элементом
+        return temp;
     }
 
     // удалить число из последовательности по значению
     // 3, 4, 5 -> removeByValue(5) -> 3, 4
     public void deleteByValue(int value) {
-        sequence.remove(Integer.valueOf(value));
+        for (int i = 0; i < sequence.length; i++) {
+            if (sequence[i] == value) {
+                sequence[i] = -2;
+                break;
+            }
+        }
+
+        sequence = shiftSequence();
         notifyObserver("removeByValue", value);
     }
 
     // получить элемент по индексу
     public void getByIndex(int index) {
-        int currentNum = sequence.get(index);
-        notifyObserver("getByIndex", currentNum);
+        if (index < sequence.length) {
+            int fountNum = sequence[index];
+            notifyObserver("getByIndex", fountNum);
+        } else {
+            System.out.println("Index out of bounds!");
+        }
     }
 
     // вставить элемент в индекс - replace()
     public void replace(int index, int value) {
-        sequence.add(index, value);
-        notifyObserver("REPLACE", index);
+        int currentNum = sequence[index];
+        sequence[index] = value;
+        notifyObserver("REPLACE " + currentNum, value);
     }
 
     //добавить число по индексу, остальные сдвинуть.
     public void insert(int index, int value) {
-        sequence.add(index, value);
-        notifyObserver("INSERT", index);
+        int indexOfTemp = 0;
+        int[] temp = new int[sequence.length + 1];
+        for (int i = 0; i < sequence.length; i++) {
+            if (i == index) {
+                int remembered = sequence[index];
+                temp[indexOfTemp++] = value;
+                temp[indexOfTemp++] = remembered;
+            } else {
+                temp[indexOfTemp++] = sequence[i];
+            }
+        }
+        sequence = temp;
+        notifyObserver("INSERT by Index " + index, value);
     }
 
     @Override
@@ -86,6 +150,23 @@ public class Sequence implements Observable {
     public void notifyObserver(String event, int value) {
         for (Observer observer : observers) {
             observer.update(event, value);
+        }
+    }
+
+    public class SequenceIterator {
+        //написать итератор по самой последовательности, нужно заменить последовательность на массив
+        private int[] sequence;
+
+        public SequenceIterator() {
+            this.sequence = Sequence.this.sequence;
+        }
+
+        public void printSequence() {
+            System.out.print(sequenceId + " Sequence: ");
+            for (int i = 0; i < sequence.length; i++) {
+                System.out.print(sequence[i] + " ");
+            }
+            System.out.println();
         }
     }
 }
